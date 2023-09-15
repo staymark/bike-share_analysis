28ab7aec8e3d3b1894c7db4b853d72718e0c5a82
mark <140750501+staymark@users.noreply.github.com>
2023-09-15 13:23:30 -0700

    additional cleaning and started analysis

--- bikeshare_analysis.R
+++ bikeshare_analysis.R
@@ -44,23 +44,50 @@ combined_data$ended_at <- ymd_hms(combined_data$ended_at)
 
 # add 'day' column
 combined_data <- combined_data %>% 
-  mutate(day = weekdays(started_at), .before = started_at)
+  mutate(day = wday(as.Date(started_at), label = TRUE), .before = started_at)
 
 # add 'ride_length' column
 combined_data <- combined_data %>% 
-  mutate(ride_length = as_hms(difftime(ended_at, started_at)), 
+  mutate(ride_length = difftime(ended_at, started_at, units = "mins"), 
          .after = ended_at)
+
+# min ride length
+min(combined_data$ride_length)
+
+# max ride length
+max(combined_data$ride_length)
+
+# looking for negative ride lengths 
+weird_min <- filter(combined_data, ride_length < 0)
+werid_min
+
+# looking for extremely long ride lengths (longer than 1 day) (1 day = 1440 min)
+weird_max <- filter(combined_data, ride_length > 1440)
+weird_max
+
+# remove ride lengths longer that are negative or longer than 1 day
+combined_data_subset <- subset(combined_data, ride_length > 0 & ride_length < 1440)
+min(combined_data$ride_length)
+max(combined_data$ride_length)
 
 # export 'combined_data' as a csv for future use
 # (don't forget to add file title at the end of path)
 # write_csv(combined_data, "/ENTER FILE PATH HERE/combined_data.csv")
-
+write_csv(combined_data, "/Users/marknival/Desktop/Analytics/Bikeshare_Case_Study/combined_data.csv")
+write_csv(combined_data_subset, "/Users/marknival/Desktop/Analytics/Bikeshare_Case_Study/combined_data_subset.csv")
 # analysis ---------------------------------------------------------------------
 
 # load cleaned data if necessary
-# combined_data <- read.csv("combined_data.csv)
+# combined_data <- read_csv("combined_data.csv")
 
 # business question: how do members and casuals differ in their usage of the bikes?
 
+# mean ride length
+mean_ride_length <- combined_data %>% 
+  aggregate(x = ride_length ~ user_type, FUN = mean)
+mean_ride_length
 
-
+# most popular day
+mode_day <- combined_data %>% 
+  aggregate(x = day ~ user_type, FUN = mode)
+# *** mode does not seem to work with non-numeric values
